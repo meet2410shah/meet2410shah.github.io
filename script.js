b9814125bcf6081009dda47e47ca290808df6507
Meet Shah <44209311+meet2410shah@users.noreply.github.com>
2020-03-22 19:57:45 +0530

    Counter Uploaded

---
+++ script.js
@@ -0,0 +1,44 @@
+const days = document.querySelector('#days');
+const hours = document.querySelector('#hours');
+const minutes = document.querySelector('#minutes');
+const seconds = document.querySelector('#seconds');
+
+const time = new Date("24 October 2022").getTime();
+let current_time = Date.now();
+let dif = time - current_time;
+
+let secs = parseInt(dif / 1000) % 60;
+let mins = parseInt(parseInt(dif / (1000*60)) % 60);
+let hs = parseInt(parseInt(dif / (1000*60*60)) % 60);
+let ds = parseInt(dif/(1000*60*60*24));
+
+seconds.innerHTML = secs;
+minutes.innerHTML = mins;
+hours.innerHTML = hs;
+days.innerHTML = ds;
+
+const Counter = setInterval(() => {
+    let ss =  parseInt(seconds.innerHTML) - 1;
+    let mm = parseInt(minutes.innerHTML);
+    let hh = parseInt(hours.innerHTML);
+    let dd = parseInt(days.innerHTML);
+    if(ss < 0) {
+        ss = 59;
+        mm--;
+        if(mm < 0) {
+            mm = 59;
+            hh--;
+            if(hh < 0) {
+                hh = 23;
+                dd--;
+            }
+        }
+    }
+    if(dd < 0) {
+        clearInterval(Counter);
+    }
+    seconds.innerHTML = ss;
+    minutes.innerHTML = mm;
+    hours.innerHTML = hh;
+    days.innerHTML = dd;
+}, 1000);
