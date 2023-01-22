a39accabb709fc340589203a6a3394c565896f08
Meet Shah <meet2410shah@gmail.com>
2023-01-22 22:14:42 +0530

    script modified

--- script.js
+++ script.js
@@ -4,20 +4,20 @@ const minutes = document.querySelector('#minutes');
 const seconds = document.querySelector('#seconds');
 
 const time = new Date('22 September 2027').getTime();
-let current_time = Date.now();
-let dif = time - current_time;
-
-let secs = parseInt(dif / 1000) % 60;
-let mins = parseInt(parseInt(dif / (1000 * 60)) % 60);
-let hs = parseInt(parseInt(dif / (1000 * 60 * 60)) % 24);
-let ds = parseInt(dif / (1000 * 60 * 60 * 24));
-
-seconds.innerHTML = secs;
-minutes.innerHTML = mins;
-hours.innerHTML = hs;
-days.innerHTML = ds;
 
 const Counter = setInterval(() => {
+	let current_time = Date.now();
+	let dif = time - current_time;
+
+	let secs = parseInt(dif / 1000) % 60;
+	let mins = parseInt(parseInt(dif / (1000 * 60)) % 60);
+	let hs = parseInt(parseInt(dif / (1000 * 60 * 60)) % 24);
+	let ds = parseInt(dif / (1000 * 60 * 60 * 24));
+
+	seconds.innerHTML = secs;
+	minutes.innerHTML = mins;
+	hours.innerHTML = hs;
+	days.innerHTML = ds;
 	let ss = parseInt(seconds.innerHTML)--;
 	let mm = parseInt(minutes.innerHTML);
 	let hh = parseInt(hours.innerHTML);
