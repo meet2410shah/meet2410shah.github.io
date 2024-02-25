0bfd53506b0a855a73ab97a91a93c12f0d57a7e6
Meet Shah <meet2410shah@outlook.com>
2024-02-25 09:47:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const hours = document.querySelector('#hours');
 const minutes = document.querySelector('#minutes');
 const seconds = document.querySelector('#seconds');
 
-const time = new Date('5 March 2024').getTime();
+const time = new Date('1 November 2026').getTime();
 
 const Counter = setInterval(() => {
 	let current_time = Date.now();
